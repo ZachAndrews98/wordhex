0670d1c7f1eb74c2cdff8a1c86d7ce6eab2a956b
zach <andrewsz@allegheny.edu>
2018-02-27 01:56:18 -0500

    Initial commit, everything "works"

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # wordHex
+
+Converts inputted word or phrase into hexadecimal color groups. Inspired by
+Numberphile's YouTube video on Illegal Numbers. Works wellish (for someone with
+a little experience writing in python), probably could be done better.
+
+## Execution
+
+Fairly straight forward, just execute like any other python3 program.
+
+```
+python3 wordHex.py
+```
+
+User will be prompted to enter a word to convert to hex. Type in word or phrase
+(no real limit but after a certain point colors are not visible with current
+implementation). After this Turtle will take over and draw each Hexcolor on the
+canvas and will write out the extra hexcode that was not 6 characters long if
+one exists. Then the user will be prompted if they want to save the canvas
+(simple yes/no question). If yes, then the user will be asked to enter a file
+name, the canvas will then be saved in a .ps file with that name, if a file with
+that name already exists, the old file will be overwritten.
+
+## Improvements
+
+If don't end up getting bored with this I'll probably try and improve it myself.
+However, on the off chance that someone stumbled upon this, one- what are you
+doing here?, and on the even smaller chance that you want to improve on this
+system (which probably won't be hard to do), go right ahead. I'm definitely not
+the first person to do this, so take it, modify it, make it hundreds of times
+better. Just let me know if you do, I want to see where I could have done
+something different/better cause its all just about learning and getting better.
---
+++ wordHex.py
@@ -0,0 +1,70 @@
+import turtle
+# dictionary of 4 digit binary numbers and their hex equivalent
+hex_keys = {
+    "0000":"0","0001":"1","0010":"2","0011":"3","0100":"4","0101":"5","0110":"6",
+    "0111":"7","1000":"8","1001":"9","1010":"A","1011":"B","1100":"C","1101":"D",
+    "1110":"E","1111":"F"}
+
+if __name__ == "__main__":
+    word = list(str(input("Enter the word you would like to convert to hex\n")))
+    binary = ""
+    # convert word/phrase to binary equivalent
+    for letter in word:
+        print("'" + letter + "'= 0" + format(ord(letter),'b'))
+        binary += "0"+ format(ord(letter),'b')
+    print("Decimal Number: " + str(int(binary,2)))
+    print("Binary Number: " + binary)
+    # breaks up binary string into groups of 4 digits
+    binary = [binary[i:i + 4] for i in range(0, len(binary), 4)]
+    # adds 0s to beginning of any groups that have less than 4 digits
+    for x in range(len(binary)):
+        if len(binary[x]) < 4:
+            while len(binary[x]) < 4:
+                binary[x] = "0" + binary[x]
+    print("Binary Groups", end=': ')
+    print(binary)
+    hex_dec = ""
+    # converts binary groups into their hex equivalents
+    for letter in binary:
+        hex_dec += hex_keys[letter]
+    print("Hex Number: "+hex_dec)
+    # breaks hex letters into groups of 6 (hexcolors)
+    hex_dec = [hex_dec[i:i + 6] for i in range(0, len(hex_dec), 6)]
+    print("Hex Groups", end=': ')
+    print(hex_dec)
+    # turtle operations for drawing each color onto the canvas
+    display = turtle.Screen()
+    pen = turtle.Turtle()
+    pen.speed(0)
+    display.tracer(0,0)
+    pen.up()
+    pen.goto(-300,-100)
+    length = (int)(display.screensize()[0]/len(hex_dec))
+    width = (int)(display.screensize()[1])
+    for color in hex_dec:
+        if len(color) is 6:
+            pen.fillcolor('#'+color)
+            pen.down()
+            pen.begin_fill()
+            pen.forward(length)
+            pen.left(90)
+            pen.forward(width)
+            pen.left(90)
+            pen.forward(length)
+            pen.left(90)
+            pen.forward(width)
+            pen.left(90)
+            pen.forward(length)
+            pen.up()
+            pen.end_fill()
+        else:
+            pen.write('+'+color,True,align="left",font=("Arial",16,"normal"))
+            pen.up()
+            pen.forward(10)
+    # allows user to save canvas as a .ps file (too lazy to do nice file type like .png)
+    save = str(input("Would you like to save the image (yes/no)?\n"))
+    if save == "yes":
+        cv = display.getcanvas()
+        name = str(input("What would you like to name the file?\n"))
+        cv.postscript(file= name+".ps", colormode='color')
+    display.exitonclick()
